11d29f57edc5539fd79221d9701270ff398c15bf
unknown <summersd@devinsummers15.MaristSchool.Local>
2015-03-03 21:56:28 -0500

    More 3/3/15 edits

--- BaseballStatTracker/src/com/example/baseballstattracker/Player.java
+++ BaseballStatTracker/src/com/example/baseballstattracker/Player.java
@@ -3,11 +3,48 @@ package com.example.baseballstattracker;
 public class Player {
 	
 	//Fields
-	
+	private int playerId;
+	private String name;
+	private String team;
+	private String atBats;
+	private String baseOne;
+	private String baseTwo;
+	private String baseThree;
+	private String homeRun;
+	private String hit;
+	private String baseOnBalls;
+	private String hitByPitch;
+	private String plateAppearances;
+	private String sacrificeHit;
+	private String sacrificeFly;
+	private String groundIntoDouble;
+	private String fieldersChoice;
+	private String run;
+	private String rbi;
+	private String stolenBase;
+	private String battingAverage;
+	private String extraBaseHit;
+	private String totalAverage;
+	private String paso;
+	private String stolenBaseAverage;
+	private String stolenBasePercentage;
+	private String isolatedPower;
+	private String onBasePercentage;
+	private String totalBases;
+	private String slugging;
+	private String timesOnBase;
 	
 	//Constructor
 	public Player() {
 		
 	}
+	
+	//Modifiers
+	public void setPlayerId(int id) {
+		playerId = id;
+	}
+	
+	
+	//Accessors
 
 }
--- BaseballStatTracker/src/com/example/baseballstattracker/PlayersDAO.java
+++ BaseballStatTracker/src/com/example/baseballstattracker/PlayersDAO.java
@@ -1,6 +1,11 @@
 package com.example.baseballstattracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -51,5 +56,76 @@ public class PlayersDAO {
 		dbHelper.close();
 	}
 			
+	public Team createPlayer(Player p) {
+		ContentValues values = new ContentValues();
+		// Place t data into values object
+		// Handle boolean values
+		//values.put(PlayersSQLiteHelper.COLUMN_NAME, p.getName());
+		
+		
+		long insertId = database.insert(PlayersSQLiteHelper.TABLE_PLAYERS, null, values);
+		
+		Cursor cursor = database.query(PlayersSQLiteHelper.TABLE_PLAYERS, allColumns,
+										PlayersSQLiteHelper.COLUMN_ID + " = " + insertId , null, null, null, null);
+		cursor.moveToLast();
+		Team newTeam = cursorToTeam(cursor);
+		cursor.close();
+		
+		return newTeam;
+	}
+	
+	public void deleteTeam(Team t) {
+		int id = t.getId();
+		database.delete(TeamsSQLiteHelper.TABLE_TEAMS,
+				TeamsSQLiteHelper.COLUMN_ID + " = " + id, null);
+	}
+	
+	public void editTeam(Team t) {
+		ContentValues values = new ContentValues();
+		int id = t.getId();
+		
+		values.put(TeamsSQLiteHelper.COLUMN_NAME, t.getName());
+		values.put(TeamsSQLiteHelper.COLUMN_PLAYERS, t.getPlayers());
+		
+		database.update(TeamsSQLiteHelper.TABLE_TEAMS, values, TeamsSQLiteHelper.COLUMN_ID + " = " + id, null);
 
+	}
+	
+	public List<Team> getAllTeams() {
+		List <Team> teamList = new ArrayList<Team>(0);
+		Cursor cursor = database.query(TeamsSQLiteHelper.TABLE_TEAMS, allColumns, null, null, null, null, null);
+		
+		cursor.moveToFirst();
+		
+		while(!cursor.isAfterLast()) {
+			Team team = cursorToTeam(cursor);
+			teamList.add(team);
+			cursor.moveToNext();
+		}
+
+		return teamList;
+		
+	}
+	
+	private Team cursorToTeam(Cursor cursor) {
+		
+		int id = cursor.getInt(0);
+		String name = cursor.getString(1);
+		String players = cursor.getString(2);
+		
+		Team t = new Team();
+		
+		t.setTeamId(id);
+		t.setName(name);
+		t.addPlayer(players);
+		
+		return t;		
+	}
+	
+	public Team getTeamById(int id) {
+		//Create a cursor
+		Cursor cursor = database.query(TeamsSQLiteHelper.TABLE_TEAMS, allColumns,
+				TeamsSQLiteHelper.COLUMN_ID + " = " + id, null, null, null, null);
+		return (cursor.moveToFirst()) ? cursorToTeam(cursor) : null;
+	}
 }
